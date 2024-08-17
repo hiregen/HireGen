59b51740dc6d94ccb6d355d3c21848a77238e0bc
HireGen <hire.gen.academy@gmail.com>
2024-08-17 14:17:34 +0100

    Fix commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/HireGen/",
-  assetsInclude: ["**/*.PNG"],
+  assetsInclude: ["**/*.png", "**/*.PNG"],  // Consider adding both lowercase and uppercase if needed
 })
